3ae48048c96f3ba0f28b7ace008d8a6a62093c9b
sintrode <562c3fb8@opayq.com>
2024-01-30 13:15:38 -0500

    Indicate unique words
    List all words that contain a letter/position combination that appears only once in the findstr output, then update the longest columns accordingly

--- ts_solve.bat
+++ ts_solve.bat
@@ -41,7 +41,12 @@ call :validateDictionary || goto :fatalError
 set "search_string=^[%*]$"
 set "search_string=!search_string: =][!"
 
-findstr /r /c:"!search_string!" 2of12inf.txt
+call :columnToArray %*
+call :iterateOverLargestColumns !column_max!
+
+set "search_string="
+for /L %%A in (0,1,%column_count%) do set "search_string=!search_string![!columns[%%A]!]"
+findstr /r /c:"^^!search_string!$" 2of12inf.txt
 exit /b
 
 ::------------------------------------------------------------------------------
@@ -77,6 +82,103 @@ if not exist "%wordlist%" (
 exit /b 0
 
 ::------------------------------------------------------------------------------
+:: Counts the number of characters in each column and stores the largest value
+::
+:: Arguments: %* - All column lists as given by the player
+:: Returns:   None
+::------------------------------------------------------------------------------
+:columnToArray
+set "column_count=-1"
+set "column_max=0"
+for %%A in (%*) do (
+    set /a column_count+=1
+    set "columns[!column_count!]=%%A"
+    call :getLength %%A length
+    set "col_lengths[!column_count!]=!length!"
+    if !length! GTR !column_max! (
+        set "column_max=!length!"
+        set "longest_columns=!column_count!"
+    ) else if !length! EQU !column_max! (
+        set "longest_columns=!longest_columns! !column_count!"
+    )
+)
+exit /b
+
+::------------------------------------------------------------------------------
+:: strLen7 by dbenham
+:: Calculates the length of a string and stores that value in a variable
+:: https://ss64.org/viewtopic.php?pid=6478#p6478
+::
+:: Arguments: %1 - The string to calculate the length of
+::            %2 - The variable to store the length in
+:: Returns:   None
+::------------------------------------------------------------------------------
+:getLength
+setlocal EnableDelayedExpansion
+set "s=%~1"
+set "len=0"
+for %%N in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
+	if "!s:~%%N,1!" neq "" (
+		set /a "len+=%%N"
+		set "s=!s:~%%N!"
+	)
+)
+endlocal&if "%~2" neq "" (set %~2=%len%) else echo %len%
+exit /b
+
+::------------------------------------------------------------------------------
+:: Iterates over the largest columns to generate regex patterns that contain
+:: one character in the relevant field
+::
+:: Arguments: %1 - The length of the longest columns
+:: Returns:   None
+::------------------------------------------------------------------------------
+:iterateOverLargestColumns
+echo UNIQUE
+echo ------
+for /L %%A in (0,1,%column_count%) do (
+    set /a "before_col=%%A-1", "after_col=%%A+1"
+    
+    for /L %%B in (0,1,%column_max%) do (
+        set "iterated_pattern=["
+        for /L %%C in (0,1,!before_col!) do set "iterated_pattern=!iterated_pattern!!columns[%%~C]!]["
+        set "iterated_pattern=!iterated_pattern!!columns[%%A]:~%%B,1!]["
+        for /L %%C in (!after_col!,1,!column_count!) do set "iterated_pattern=!iterated_pattern!!columns[%%~C]!]["
+        set "iterated_pattern=^^!iterated_pattern:~0,-1!$"
+
+        for /f %%C in ('findstr /r /c:"!iterated_pattern!" "%wordlist%" ^| find /v /c ""') do (
+            if "%%C"=="1" (
+                for /f %%D in ('findstr /r /c:"!iterated_pattern!" "%wordlist%"') do (
+                    echo %%D
+                    call :stripUniqueChars "%%D"
+                )
+            )
+        )
+    )
+)
+
+echo.
+echo POSSIBLE
+echo --------
+exit /b
+
+::------------------------------------------------------------------------------
+:: If any unique words are found, remove any characters in that word that are
+:: in the longest columns to remove duplicate or otherwise incorrect entries
+::
+:: Arguments: %1 - The word whose characters will be removed
+:: Returns:   None
+::------------------------------------------------------------------------------
+:stripUniqueChars
+set "temp_string=%~1"
+for %%E in (%longest_columns%) do (
+    for /f "delims=" %%F in ("!temp_string:~%%E,1!") do (
+        set "columns[%%E]=!columns[%%E]:%%~F=!"
+    )
+)
+exit /b
+
+::------------------------------------------------------------------------------
 :: Displays the usage text for the script
 ::
 :: Arguments: None
@@ -95,4 +197,5 @@ exit /b
 echo ERROR
 echo There was an issue downloading and extracting the wordlist.
 echo Please manually download and extract http://wordlist.aspell.net/12dicts/
+pause
 exit /b
